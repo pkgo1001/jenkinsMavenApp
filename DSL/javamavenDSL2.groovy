4114129f99fa0511bde2b8e8750c8a3cf2383688
pkgo1001 <pokegoacc1001@gmail.com>
2025-11-05 22:54:07 -0600

    Refactor Java Maven App DSL configuration
    Updated job name and description, changed SCM repository and git config, modified shell command path, and replaced Slack notifier with mailer.

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
-job('Java Maven App DSL 2') {
-    description('Java Maven App con DSL para el curso de Jenkins')
+job('Java Maven App DSL') {
+    description('Job creado desde el DSL en el repositorio de Git')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/pkgo1001/jenkinsMavenApp.git', 'main') { node ->
+            node / gitConfigName('pkgo1001')
+            node / gitConfigEmail('pokegoacc1001@gmail.com')
         }
     }
     steps {
@@ -17,28 +17,12 @@ job('Java Maven App DSL 2') {
         }
         shell('''
           echo "Entrega: Desplegando la aplicación" 
-          java -jar "/var/jenkins_home/workspace/Java Maven App DSL 2/target/my-app-1.0-SNAPSHOT.jar"
+          java -jar "/var/jenkins_home/workspace/Java Maven App DSL/target/my-app-1.0-SNAPSHOT.jar"
         ''')  
     }
     publishers {
         archiveArtifacts('target/*.jar')
         archiveJunit('target/surefire-reports/*.xml')
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-       }
+      	mailer('pokegoacc1001@gmail.com', false, false)
     }
 }
